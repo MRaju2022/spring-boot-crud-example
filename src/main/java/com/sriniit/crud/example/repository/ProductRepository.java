dc8529e957666f86603638cf28df10b31a7b3869
Raju <rajumjava9@gmail.com>
2024-02-22 18:30:59 +0530

    spring-boot-crud-example

---
+++ src/main/java/com/sriniit/crud/example/repository/ProductRepository.java
@@ -0,0 +1,9 @@
+package com.sriniit.crud.example.repository;
+
+import com.sriniit.crud.example.entity.Product;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ProductRepository extends JpaRepository<Product,Integer> {
+    Product findByName(String name);
+}
+
